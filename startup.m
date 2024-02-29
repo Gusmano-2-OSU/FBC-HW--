b8ecb512545586d5641342dd37e536aab5641d28
Gusmano-2-OSU <155201655+Gusmano-2-OSU@users.noreply.github.com>
2024-02-29 11:55:25 -0500

    Include Startup File for Prettier Plots

---
+++ startup.m
@@ -0,0 +1,6 @@
+% Makes Figures Pretty
+set(groot, 'defaulttextinterpreter','latex');  
+set(groot, 'defaultAxesTickLabelInterpreter','latex');  
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot, 'defaultLineLineWidth', 1.5);
+% set(groot, 'defaultColorBarLabelInterpreter','latex');
